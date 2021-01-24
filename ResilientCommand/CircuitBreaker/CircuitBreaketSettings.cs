0193b13ae65e0cae21e040d4e620f1d9c327d483
Alex W. Carlsen <alekcarlsen@gmail.com>
2021-01-24 18:22:59 +0100

    move circuitbreaker to folder

--- ResilientCommand/CircuitBreaker/CircuitBreaketSettings.cs
+++ ResilientCommand/CircuitBreaker/CircuitBreaketSettings.cs
@@ -4,27 +4,6 @@ using System.Text;
 
 namespace ResilientCommand
 {
-    public sealed class ExecutionTimeoutSettings
-    {
-        public ExecutionTimeoutSettings(
-            bool isEnabled = Default.IsEnabled,
-            int executionTimeoutInMiliseconds = Default.ExecutionTimeoutInMiliseconds)
-        {
-            IsEnabled = isEnabled;
-            ExecutionTimeoutInMiliseconds = executionTimeoutInMiliseconds;
-        }
-
-        public bool IsEnabled { get; private set; }
-        public int ExecutionTimeoutInMiliseconds { get; private set; }
-
-        public sealed class Default
-        {
-            public const bool IsEnabled = true;
-            public const int ExecutionTimeoutInMiliseconds = 10000;
-        }
-        
-        public static ExecutionTimeoutSettings DefaultExecutionTimeoutSettings => new ExecutionTimeoutSettings();
-    }
     public sealed class CircuitBreakerSettings
     {
         public CircuitBreakerSettings(
