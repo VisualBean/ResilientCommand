88bf6f71340cc51378e8ee485321d7d363ca16df
Alex W. Carlsen <alekcarlsen@gmail.com>
2021-01-24 19:20:10 +0100

    refactor example

--- ResilientCommand.Examples/Program.cs
+++ ResilientCommand.Examples/Program.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace something.Examples
 {
-
+    // Create a ResilientCommandEventNotifier implementation.
     public class ConsoleEventNotifier : ResilientCommandEventNotifier
     {
         public override void markEvent(ResillientCommandEventType eventType, CommandKey commandKey)
@@ -14,38 +14,56 @@ namespace something.Examples
         }
     }
 
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Set the new implementation as the notifier to be used.
+            EventNotifierFactory.GetInstance().SetEventNotifier(new ConsoleEventNotifier());
+
+            // Create an instance of our command.
+            var command = new MySuperCommand();
+
+            // Run it a couple of times.
+            await command.ExecuteAsync(default);
+            await command.ExecuteAsync(default);
+            await command.ExecuteAsync(default);
+
+            /* Output: 
+            1: First call.
+                MySuperCommand: ExceptionThrown
+                MySuperCommand: FallbackSuccess
+                
+            2: Second call, trips the circuit.
+                MySuperCommand: CircuitBroken
+                MySuperCommand: ExceptionThrown
+                MySuperCommand: FallbackSuccess
+
+            3: Third call, goes directly to the fallback.
+                MySuperCommand: FallbackSuccess
+            */
+        }
+    }
+
+
     class MySuperCommand : ResilientCommand<string>
     {
-        public MySuperCommand() : base(configuration: CommandConfiguration.CreateConfiguration(config => 
+        public MySuperCommand() : base(configuration: CommandConfiguration.CreateConfiguration(config =>
         {
+            // Setup circuitbreaker to be broken after 2 tries and 10% failures.
             config.CircuitBreakerSettings = new CircuitBreakerSettings(failureThreshhold: 0.1, minimumThroughput: 2);
         }))
         {
-
         }
         protected override Task<string> RunAsync(CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
 
+        // Enabled fallback.
         protected override string Fallback()
         {
-            return "Oh noooooo!";
-        }
-    }
-
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            // setup
-            EventNotifierFactory.GetInstance().SetEventNotifier(new ConsoleEventNotifier());
-
-            var command = new MySuperCommand();
-            await command.ExecuteAsync(default);
-            await command.ExecuteAsync(default);
-            await command.ExecuteAsync(default);
-            await command.ExecuteAsync(default);
+            return "A fallback response";
         }
     }
 }
