fe8ebb96524dbe463e1c0be2e5c0f2f5b67cad75
Alex Wichmann <alexander.wichmann.carlsen@lego.com>
2021-05-18 15:52:16 +0200

    fix interface usage

--- ResilientCommand/IExecutionPolicy.cs
+++ ResilientCommand/IExecutionPolicy.cs
@@ -1,15 +1,25 @@
-﻿// <copyright file="ExecutionDecorator.cs" company="Visualbean">
+﻿// <copyright file="IExecutionPolicy.cs" company="Visualbean">
 // Copyright (c) Visualbean. All rights reserved.
 // </copyright>
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace ResilientCommand
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The interface for ExecutionPolicies.
+    /// </summary>
     public interface IExecutionPolicy
     {
+        /// <summary>
+        /// Executes the asynchronous.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="innerAction">The inner action.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A Task.</returns>
         Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> innerAction, CancellationToken cancellationToken);
     }
 }
