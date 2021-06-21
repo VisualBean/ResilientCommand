35601d9318659db01755855da3725e78d64457a9
Alex Wichmann <alexander.wichmann.carlsen@lego.com>
2021-06-21 12:02:41 +0200

    moved public before internal

--- ResilientCommand/CommandConfigurationManager.cs
+++ ResilientCommand/CommandConfigurationManager.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CommandConfiguration.cs" company="Visualbean">
+﻿// <copyright file="CommandConfigurationManager.cs" company="Visualbean">
 // Copyright (c) Visualbean. All rights reserved.
 // </copyright>
 
@@ -6,28 +6,10 @@ namespace ResilientCommand
 {
     using System;
     using System.Collections.Concurrent;
-    using static ResilientCommand.CircuitBreakerSettings;
-    using static ResilientCommand.CollapserSettings;
-    using static ResilientCommand.ExecutionTimeoutSettings;
-    using static ResilientCommand.FallbackSettings;
-    using static ResilientCommand.SemaphoreSettings;
 
     public sealed class CommandConfigurationManager
     {
         private static readonly ConcurrentDictionary<CommandKey, Lazy<CommandConfiguration>> ConfigurationByKey = new ConcurrentDictionary<CommandKey, Lazy<CommandConfiguration>>();
-
-        /// <summary>
-        /// Gets or creates a circuit breaker.
-        /// </summary>
-        /// <param name="commandKey">The command key.</param>
-        /// <param name="configuration">The  settings.</param>
-        /// <returns>
-        /// A <see cref="CommandConfiguration" />.
-        /// </returns>
-        internal static CommandConfiguration GetOrCreateCommandConfiguration(CommandKey commandKey, CommandConfiguration configuration)
-        {
-            return ConfigurationByKey.GetOrAdd(commandKey, new Lazy<CommandConfiguration>(() => configuration)).Value;
-        }
 
         /// <summary>
         /// Gets or creates a circuit breaker.
@@ -44,6 +26,19 @@ namespace ResilientCommand
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Gets or creates a circuit breaker.
+        /// </summary>
+        /// <param name="commandKey">The command key.</param>
+        /// <param name="configuration">The  settings.</param>
+        /// <returns>
+        /// A <see cref="CommandConfiguration" />.
+        /// </returns>
+        internal static CommandConfiguration GetOrCreateCommandConfiguration(CommandKey commandKey, CommandConfiguration configuration)
+        {
+            return ConfigurationByKey.GetOrAdd(commandKey, new Lazy<CommandConfiguration>(() => configuration)).Value;
         }
     }
 }
